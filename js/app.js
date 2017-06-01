2351682a49cb88705058b80e1d6eb70c1c631a45
pilarfc <pilar.fc@outlook.com>
2017-06-01 16:06:50 +0000

    agregando funcion para deshabilitar boton

--- js/app.js
+++ js/app.js
@@ -1,14 +1,18 @@
 // Cuando carga la página
 window.addEventListener("load", function () {
+     
 	// Envío de To - Do
 	var toDoForm = document.getElementById("to-do-form");
 	toDoForm.addEventListener("submit", function (e) {
 		e.preventDefault();
+        
+        
 		// Obtenemos datos
 		var contenedor = document.getElementById("posts");
 		var mensajeContenedor = document.getElementById("message");
 		var mensaje = mensajeContenedor.value;
-
+         
+        
 		// Creamos elementos
 		var postContenedor = document.createElement("article");
 		var post = document.createElement("p");
@@ -26,12 +30,50 @@ window.addEventListener("load", function () {
 		// Borrar contenido de textarea
 		mensajeContenedor.value = "";
 	});
+    
 });
 
 
+var boton = document.getElementById("boton-agregar"); 
+var mensajeVacio = document.getElementById("message");
+
+
+    
+   boton.setAttribute("disabled", "disabled"); 
+   mensajeVacio.addEventListener ("keypress",validar);
+    
+function validar () {
+       
+       if (this.value != "") {
+        boton.removeAttribute("disabled"); 
+    } else {
+        boton.setAttribute("disabled", "disabled")
+    }
+       
+ }
 
 
 
+
+
+
+/*
+$(document).ready(function(){
+
+     $('input[type="submit"]').attr('disabled','disabled');
+
+    $('input[type="text"]').keypress(function(){
+
+            if($(this).val() != ''){
+
+               $('input[type="submit"]').removeAttr('disabled');
+
+            }
+
+     });
+
+ });
+*/
 
 
 
